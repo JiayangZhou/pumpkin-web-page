b59b6cfdc5bc8dc787d14f1c6cf7a3415708fcfd
root <root@AZ-VM-TS-1.qrbgq2aeipxuvfrzvvl1zhlisf.fx.internal.cloudapp.net>
2021-10-10 18:42:33 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to our front page
+          - Yuning, Leilei, Jiayang
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/JiayangZhou"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Contact us
         </a>
       </header>
     </div>
